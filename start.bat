8ccc0200757d1cbb789181cfc98c50f862b97bb1
Rachit Pal <83032665+Rachit-Pal@users.noreply.github.com>
2022-07-08 15:49:21 +0530

    Anya Forger

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Saitama Robot
-:: Enables virtual env mode and then starts saitama
-env\scripts\activate.bat && py -m SaitamaRobot
+TITLE SophiaBot
+:: Enables virtual env mode and then starts Rika
+env\scripts\activate.bat && py -m SophiaBot
